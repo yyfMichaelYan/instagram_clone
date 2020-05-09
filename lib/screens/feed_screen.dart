eac82153ce0650f5480f98f29aa2559d74d2934b
yyfMichaelYan <yyfmichaelyan@gmail.com>
2020-05-08 20:03:29 -0500

    profile posts

--- lib/screens/feed_screen.dart
+++ lib/screens/feed_screen.dart
@@ -1,10 +1,8 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/models/post_model.dart';
 import 'package:instagram_clone/models/user_model.dart';
-import 'package:instagram_clone/screens/profile_screen.dart';
-import 'package:instagram_clone/services/auth_service.dart';
 import 'package:instagram_clone/services/database_service.dart';
+import 'package:instagram_clone/widgets/post_view.dart';
 
 class FeedScreen extends StatefulWidget {
   static final String id = 'feed_screen';
@@ -30,104 +28,6 @@ class _FeedScreenState extends State<FeedScreen> {
     setState(() {
       _posts = posts;
     });
-  }
-
-  _buildPost(Post post, User author) {
-    return Column(
-      children: <Widget>[
-        GestureDetector(
-          onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (_) => ProfileScreen(
-                        currentUserId: widget.currentUserId,
-                        userId: author.id,
-                      ))),
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-            child: Row(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 25.0,
-                  backgroundColor: Colors.grey,
-                  backgroundImage: author.profileImageUrl.isEmpty
-                      ? AssetImage('assets/images/user_placeholder.png')
-                      : CachedNetworkImageProvider(author.profileImageUrl),
-                ),
-                SizedBox(
-                  width: 8.0,
-                ),
-                Text(
-                  author.name,
-                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
-                )
-              ],
-            ),
-          ),
-        ),
-        Container(
-          height: MediaQuery.of(context).size.width,
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: CachedNetworkImageProvider(post.imageUrl),
-                  fit: BoxFit.cover)),
-        ),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.favorite_border),
-                    iconSize: 30.0,
-                    onPressed: () => {},
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.comment),
-                    iconSize: 30.0,
-                    onPressed: () => {},
-                  )
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 12.0),
-                child: Text(
-                  '0 likes',
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                ),
-              ),
-              SizedBox(
-                height: 8.0,
-              ),
-              Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.only(left: 12.0, right: 6.0),
-                    child: Text(
-                      author.name,
-                      style: TextStyle(
-                          fontSize: 16.0, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Expanded(
-                    child: Text(
-                      post.caption,
-                      style: TextStyle(fontSize: 16.0),
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  )
-                ],
-              ),
-              SizedBox(
-                height: 12.0,
-              )
-            ],
-          ),
-        )
-      ],
-    );
   }
 
   @override
@@ -156,7 +56,11 @@ class _FeedScreenState extends State<FeedScreen> {
                             return SizedBox.shrink();
                           }
                           User author = snapshot.data;
-                          return _buildPost(post, author);
+                          return PostView(
+                            currentUserId: widget.currentUserId,
+                            post: post,
+                            author: author,
+                          );
                         },
                       );
                     }),
